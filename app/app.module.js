bd31cf7540c537e4ae6a85a5bc0751b334eb4674
Omar Zaarour <zarour.omar@gmail.com>
2017-02-09 16:05:20 -0700

    add star component as nested component in product list

--- app/app.module.js
+++ app/app.module.js
@@ -10,6 +10,7 @@ var product_list_component_1 = require("./products/product-list.component");
 var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var product_filter_filter_1 = require("./products/product-filter.filter");
+var star_component_1 = require("./shared/star.component");
 var app_component_1 = require("./app.component");
 var AppModule = (function () {
     function AppModule() {
@@ -19,7 +20,7 @@ var AppModule = (function () {
 AppModule = __decorate([
     core_1.NgModule({
         imports: [platform_browser_1.BrowserModule, forms_1.FormsModule],
-        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_filter_1.ProductFilterPipe],
+        declarations: [app_component_1.AppComponent, product_list_component_1.ProductListComponent, product_filter_filter_1.ProductFilterPipe, star_component_1.StarComponent],
         bootstrap: [app_component_1.AppComponent]
     })
 ], AppModule);
--- app/app.module.ts
+++ app/app.module.ts
@@ -3,12 +3,13 @@ import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from './products/product-filter.filter';
+import { StarComponent } from './shared/star.component';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent , ProductListComponent,ProductFilterPipe],
+  declarations: [ AppComponent , ProductListComponent,ProductFilterPipe,StarComponent],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
