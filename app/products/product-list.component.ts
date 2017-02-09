5c382700c968fac95fd0499fbe4d7a07419473aa
Omar Zaarour <zarour.omar@gmail.com>
2017-02-09 13:41:01 -0700

    add styles sheet to a component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -4,8 +4,8 @@ import {Component} from "@angular/core";
 import {IProduct} from './product'
 @Component({
 selector: 'pm-products',
-templateUrl: 'app/products/product-list.component.html'
-
+templateUrl: 'app/products/product-list.component.html',
+styleUrls:['app/products/product-list.component.css']
 })
 
 export class ProductListComponent{
