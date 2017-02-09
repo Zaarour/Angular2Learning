4402c2939d7a399d6e16b530a6b348b75a044723
Omar Zaarour <zarour.omar@gmail.com>
2017-02-09 14:46:27 -0700

    create customer filter product pipe and use it

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { ProductListComponent } from './products/product-list.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { ProductFilterPipe } from './products/product-filter.filter';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports: [ BrowserModule,FormsModule ],
-  declarations: [ AppComponent , ProductListComponent],
+  declarations: [ AppComponent , ProductListComponent,ProductFilterPipe],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
