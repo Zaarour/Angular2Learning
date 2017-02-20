3e275019b72fd75e2b03581dc9c0eff7e0ec04fa
Omar Zaarour <zarour.omar@gmail.com>
2017-02-20 16:41:18 -0700

    read /write a route variable (product id)

--- app/products/product-detail.component.js
+++ app/products/product-detail.component.js
@@ -5,17 +5,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 var core_1 = require("@angular/core");
+var router_1 = require("@angular/router"); // this service is to read values of parameters in routes
 var ProductDetailComponent = (function () {
-    function ProductDetailComponent() {
+    function ProductDetailComponent(_route) {
+        this._route = _route;
         this.pageTitle = 'Product details';
+        //console.log(this._route.snapshot.params['id']);// this will get the id from the route
     }
+    ProductDetailComponent.prototype.ngOnInit = function () {
+        var id = +this._route.snapshot.params['id']; // the plus is js shortcut to convert a string to numeric
+        this.pageTitle += " :" + id; //es 2015 template string
+    };
     return ProductDetailComponent;
 }());
 ProductDetailComponent = __decorate([
     core_1.Component({
         templateUrl: 'app/products/product-detail.component.html'
-    })
+    }),
+    __metadata("design:paramtypes", [router_1.ActivatedRoute])
 ], ProductDetailComponent);
 exports.ProductDetailComponent = ProductDetailComponent;
 //# sourceMappingURL=product-detail.component.js.map
--- app/products/product-detail.component.ts
+++ app/products/product-detail.component.ts
@@ -1,12 +1,26 @@
-import  {Component} from '@angular/core';
+import  {Component,OnInit} from '@angular/core';
 import {IProduct} from './product'
-
+import {ActivatedRoute} from '@angular/router' // this service is to read values of parameters in routes
 
 @Component({
 templateUrl:'app/products/product-detail.component.html'
 })
 
-export class ProductDetailComponent{
+export class ProductDetailComponent implements OnInit{
+
+
     pageTitle:string='Product details'
     product:IProduct;
+
+    constructor(private _route:ActivatedRoute){
+        //console.log(this._route.snapshot.params['id']);// this will get the id from the route
+    }
+
+
+    ngOnInit():void{
+            let id= +this._route.snapshot.params['id']; // the plus is js shortcut to convert a string to numeric
+            this.pageTitle +=   ` :${id}`; //es 2015 template string
+    }
+
+
 }
